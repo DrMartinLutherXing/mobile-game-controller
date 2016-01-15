0948d1c8b907c852af24737d16ea87825684d513
drmartinlutherxing <drmartinlutherchang@gmail.com>
2016-01-14 21:23:34 -0800

    adjusted svg starting constants and order of card suit and value lists;

--- mobile-controller/js/holdem/constants.js
+++ mobile-controller/js/holdem/constants.js
@@ -1,15 +1,25 @@
 CONSTANTS = {
 	"card": {
+		"dims": {
+			"svg": {
+				"xStart": 500,
+				"xPadding": 42.75,
+				"yStart": 1166,
+				"yPadding": 42.75,
+				"width": 238.11,
+				"height": 332.6
+			},
+		},
 		"suit": [
-			"heart",
-			"diamond",
 			"club",
-			"spade"
+			"heart",
+			"spade",
+			"diamond"
 		],
 		"value": [
-			"2", "3", "4", "5", "6",
-			"7", "8", "9", "10", "J",
-			"Q", "K", "A"]
+			"A", "2", "3", "4", "5",
+			"6", "7", "8", "9", "10",
+			"J", "Q", "K"
 		],
 	}
 };
