fdec8b4107bf27ccad17d190ca80dc980c7d51d6
drmartinlutherxing <drmartinlutherchang@gmail.com>
2016-01-18 00:06:55 -0800

    extra space;added ui to reuire statement;removed extra .;

--- mobile-controller/js/core/ui.js
+++ mobile-controller/js/core/ui.js
@@ -1,14 +1,13 @@
-CT.require("CT.dom");
 CT.require("CT.align");
 // file called core/ui.js
 core.ui = {
 	"_ui": null,
 	"load": function(gamename) {
-		var platform = CT.align.width()  < 720 ? "mobile" : "display";
-		CT.require("games." + gamename + "." + platform, True);
-		core.ui._ui = games[gamename].ui.[platform];
+		var platform = CT.align.width() < 720 ? "mobile" : "display";
+		CT.require("games." + gamename + ".ui." + platform, true);
+		core.ui._ui = games[gamename].ui[platform];
 	},
-	"doWhatever": function() {
-		core.ui._ui.doWhatever()
+	"init": function(initial) {
+		core.ui._ui.init(initial);
 	}
 };
