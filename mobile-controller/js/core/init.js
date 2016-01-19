aea1950d3b988fc3b478947791de30da0c19f61c
drmartinlutherxing <drmartinlutherchang@gmail.com>
2016-01-19 02:42:10 -0800

    added initialization values for display ui: card[0,3,4], game_stage, pot_total, round_total, hand_number;

--- mobile-controller/js/core/init.js
+++ mobile-controller/js/core/init.js
@@ -3,13 +3,21 @@ CT.require("core.ui");
 onload = function() {
 	var view = document.getElementById("view"),
 		gamename = "holdem", initial = {
-			"table_btn": "dealer",
+			"table_btn": "big_blind",
 			"account_name": "kieran",
 			"current_money": 10000,
 			"current_bid": 100,
 			"next_bid": 100,
-			"card_1": "diamondA",
-			"card_2": "heartA"
+			"game_stage": "PREFLOP",
+			"pot_total": 2000,
+			"round_total": 1000,
+			"hand_number": 1,
+
+			"card_0": "spadeT",
+			"card_1": "spadeJ",
+			"card_2": "spadeQ",
+			"card_3": "spadeK",
+			"card_4": "spadeA"
 		};
 
 	core.ui.load(gamename);	
