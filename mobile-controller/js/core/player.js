e6840afa8ac4c9bd4c6db258cc055a9ca3ae6cf5
bubbleboy14 <mario.balibrera@gmail.com>
2016-01-18 22:36:57 -0800

    player class

--- mobile-controller/js/core/player.js
+++ mobile-controller/js/core/player.js
@@ -1,14 +1,26 @@
 CT.require("CT.pubsub");
 
-// events
-//  - pubsub: pm|message|subscribe|join|leave|open|close|error|wserror
-//  - player (additional): gamelist
-// player actions: connect|join
-// dealer actions: connect|start
+// player actions: join
+// dealer actions: start
 
-var cpcb = null;
-core.player = {
-	"_cbs": {},
+core.player = CT.Class({
+	"_cbs": {
+		// application-level callbacks
+		//  - override w/ cfg object (2nd constructor argument)
+
+		// websocket events
+		"pm": CT.log.getLogger("core.player|pm"),
+		"message": CT.log.getLogger("core.player|message"),
+		"subscribe": CT.log.getLogger("core.player|subscribe"),
+		"join": CT.log.getLogger("core.player|join"),
+		"leave": CT.log.getLogger("core.player|leave"),
+		"open": CT.log.getLogger("core.player|open"),
+		"close": CT.log.getLogger("core.player|close"),
+		"error": CT.log.getLogger("core.player|error"),
+		"wserror": CT.log.getLogger("core.player|wserror"),
+		// player event
+		"gamelist": CT.log.getLogger("core.player|gamelist")
+	},
 	"_name": null,
 	"_on_open": function() {
 		CT.log("OPEN");
@@ -16,50 +28,50 @@ core.player = {
 	},
 	"_on_close": function() {
 		CT.log("CLOSE");
-		cpcb.close && cpcb.close();
+		this._cbs.close();
 	},
 	"_on_wserror": function() {
 		CT.log("WEBSOCKET ERROR");
-		cpcb.wserror && cpcb.wserror();
+		this._cbs.wserror();
 	},
 	"_on_error": function(message) {
 		CT.log("ERROR (non-fatal): " + message);
-		cpcb.error && cpcb.error(message);
+		this._cbs.error(message);
 	},
 	"_on_subscribe": function(data) {
 		CT.log("SUBSCRIBE " + JSON.stringify(data));
 		if (data.channel == "lobby")
-			cpcb.open && cpcb.open(data);
+			this._cbs.open(data);
 		else
-			cpcb.subscribe && cpcb.subscribe(data);
+			this._cbs.subscribe(data);
 	},
 	"_on_join": function(channel, user) {
 		CT.log("JOIN " + channel + " " + user);
-		cpcb.join && cpcb.join(data);
+		this._cbs.join(data);
 	},
 	"_on_leave": function(channel, user) {
 		CT.log("LEAVE " + channel + " " + user);
-		cpcb.leave && cpcb.leave(data);
+		this._cbs.leave(data);
 	},
 	"_on_message": function(data) {
 		CT.log("MESSAGE " + JSON.stringify(data));
-		cpcb.message && cpcb.message(data);
+		this._cbs.message(data);
 	},
 	"_on_pm": function(data, user) {
 		CT.log("PM " + user + ": " + JSON.stringify(data));
 		if (user == "lobby") { // system messages
 			if (data.type == "list")
-				cpcb.gamelist && cpcb.gamelist(data.data);
+				this._cbs.gamelist(data.data);
 			else
 				throw "unimplemented lobby private message!";
 		} else // regular private message
-			cpcb.pm && cpcb.pm(data);
+			this._cbs.pm(data);
 	},
-	"connect": function(uname, cbs) {
-		core.player._name = uname;
-		cpcb = core.player._cbs = cbs || {};
+	"init": function(uname, cbs) {
+		this._name = uname;
+		this._cbs = CT.merge(cbs, this._cbs);
 		for (var key in CT.pubsub._.cb) // pubsub events
-			CT.pubsub.set_cb(key, core.player["_on_" + key]);
+			CT.pubsub.set_cb(key, this["_on_" + key]);
 		// TODO: configurize later
 		CT.pubsub.connect("localhost", 8888, uname);
 	},
@@ -81,4 +93,4 @@ core.player = {
 		CT.log("PM " + user + " " + message);
 		CT.pubsub.pm(user, message);
 	}
-};
+});
