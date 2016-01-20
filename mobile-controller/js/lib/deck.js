e295b8944a2087800f29c54aa884b991d48510c7
drmartinlutherxing <drmartinlutherchang@gmail.com>
2016-01-20 04:02:47 -0800

    consts for deck cards and values; created deck and card classes; Card class initialized with suit and value arguements. Card.val() returns str concatentation; Deck class initialization calls _build and shuffle. Deck._build creates 52 Card objects into Deck._deck based on local consts. Deck._shuffle shuffles Deck._deck cards randomly. Deck.print prints Card.val() for each card in Deck._deck. Deck.draw unimplemented;

---
+++ mobile-controller/js/lib/deck.js
@@ -0,0 +1,64 @@
+var consts = {
+	suits: [
+		"diamond",
+		"heart",
+		"spade",
+		"club"
+	],
+	values: [
+		"2", "3", "4", "5", "6",
+		"7", "8", "9", "T", "J",
+		"Q", "K", "A"
+	]
+};
+
+lib.Card = CT.Class({
+	suit: "",
+	value: "",
+
+	val: function() {
+		//maybe define as getter
+		return this.suit + this.value;
+	},
+	init: function(s, v) {
+		this.suit = s ||
+			consts.suits[Math.floor(Math.random() * consts.suits.length)];
+		this.value = v ||
+			consts.values[Math.floor(Math.random() * consts.values.length)];
+	}
+});
+
+lib.Deck = CT.Class({
+	_deck: [],
+
+	_build: function() {
+		var that = this;
+		consts.suits.forEach(function(s) {
+			consts.values.forEach(function(v) {
+				that._deck.push(new lib.Card(s, v));
+			});
+		});
+	},
+	_shuffle: function() {
+		var j, x, i = this._deck.length,
+			o = this._deck;
+		while (i) {
+			j = Math.floor(Math.random() * i);
+			x = o[--i]; o[i] = o[j]; o[j] = x;
+		}
+		return o;
+	},
+	print: function() {
+		var string = "";
+		for (var i in this._deck)
+			string += this._deck[i].val() + "\n";
+		console.log(string);
+	},
+	draw: function() {
+	},
+	init: function() {
+		this._build();
+		this._shuffle();
+
+	}
+});
