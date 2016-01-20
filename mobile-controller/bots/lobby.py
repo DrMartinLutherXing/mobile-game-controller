d637f840aec066c708e5bcede7c6a6ddd93da25a
bubbleboy14 <mario.balibrera@gmail.com>
2016-01-20 01:53:32 -0800

    Concierge supports new private messages (both from host): create, start

--- mobile-controller/bots/lobby.py
+++ mobile-controller/bots/lobby.py
@@ -5,8 +5,8 @@ from cantools.util import log
 class Lobby(pubsub.Bot):
 	def __init__(self, server, channel, name="Concierge"): # only one Concierge..
 		pubsub.Bot.__init__(self, server, channel, name)
-		self.seeks = {}
 		self.games = {}
+		self.counter = 0
 
 	def on_publish(self, data):
 		log("Concierge received publish: %s"%(json.dumps(data),))
@@ -16,13 +16,28 @@ class Lobby(pubsub.Bot):
 		self.server.pm({
 			"user": data["user"],
 			"message": {
-				"type": "list",
-				"data": self.games.keys()
+				"action": "list",
+				"data": self.games
 			}
 		}, self)
 
 	def on_unsubscribe(self, data):
 		log("Concierge received unsubscribe: %s"%(json.dumps(data),))
 
-	def on_pm(self, data):
-		log("Concierge received private message: %s"%(json.dumps(data),))
+	def on_pm(self, obj):
+		log("Concierge received private message: %s"%(json.dumps(obj),))
+		user = obj["user"]
+		message = obj["message"]
+		action = message["action"]
+		data = message["data"]
+		if action == "create":
+			gname = "%s_%s_%s"%(data, user, self.counter)
+			self.counter += 1
+			if data not in self.games:
+				self.games[data] = set()
+			self.games[data].add(gname)
+		elif action == "start":
+			gtype = data.split("_")[0]
+			if gtype in self.games:
+				if data in self.games[gtype]:
+					self.games[gtype].remove(data)
