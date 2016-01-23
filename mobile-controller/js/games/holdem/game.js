a67146ee2361795601366c172323efa9021cb27c
drmartinlutherxing <drmartinlutherchang@gmail.com>
2016-01-23 05:01:47 -0800

    update for games.holdem.game; update values for _round_bid _blind_increase_interval; change _pot_total to _pot object to track active player's entered bids; _forEachActive fires cb if the given playerData status is active; _numActive counts the number of playerData with status active; add displayObject to _data; _buildPot initialized to 0 for active players, player bid input an integer, indexed by player id in _pot; _setNextDealer sets _dealer_index to index returned from _nextActivePlayerIndex; change method from _setTableBlinds to _updateTableBlinds; added _updateUIs; _updatePlayersStatus resets fold player status to active and players with no money to inactive; _resetRound calls _buildPot and increases _round bid based on _hand_number; _distributePot takes a list of winning player ids. sorts winning player ids based on how much each put in pot. determines how much each winner gets from each pot they are included in. calls _updatePlayerCash with updated winnings or leftover pot money for all players; _rankHand takes a player object, adds a rank object onto the playerData that is an instance of the Rank class, based on the players cards in hand and the 5 display cards; _bestHandRanks compares the rank objects on all the active playerData. returns a list of player ids of the winners; _determineWinners calls _rankHand on all active players, and calls _distributePot on the result of _bestHandRanks; _deal _game_stage show callback fires _determineWinners; cash in _handlePlayerResponse defined by response cash key. update _pot object based on response; _bid _game_stage callback fires end. other _game_stages fire _bidQuery on next player; _clean calls _updatePlayersStatus and _updateUIs; _end calls _clean _setNextDealer and _start; _start now calls _resetRound. if only one player left it calls setWinner for this play on the display. otherwise it increases _hand_number, and calls _updateTableBlinds and _run;

--- mobile-controller/js/games/holdem/game.js
+++ mobile-controller/js/games/holdem/game.js
@@ -5,21 +5,34 @@ games.holdem.game =
 		"preflop", "flop",
 		"turn", "river", "show"
 	],
-	"_round_bid": 0,
+	"_round_bid": 100,
+	"_blind_increase_interval": 11
 	"_game_stage": "",
 	"_dealer_index": 0,
 	"_hand_number": 0,
 	"_round_total": 0,
-	"_pot_total": 0,
+	"_pot": {},
 	"_bid_start_index": 0,
 	"_host": null,
 	"_display": null,
 	"_players": null,
 	"_deck": null,
+
+	"_forEachActive": function(cb) {
+		var g = games.holdem.game;
+		g._players.forEach(function(p) {
+			if (g._data[p._id].stat == "active") cb(p);
+		});
+	},
+	"_numActive":  function() {
+		var g = games.holdem.game, num = 0;
+		g._forEachActive(function(){++num;});
+		return num;
+	},
 	"_build": function() {
-		var g = game.holdem.game;
+		var g = games.holdem.game;
 		g._deck = new Deck();
-		g._data[g._display.id] = {};
+		g._data[g._display._id] = {};
 		g._players.forEach(function(p) {
 			g._data[p._id] = {
 				"stat": "active",
@@ -27,7 +40,11 @@ games.holdem.game =
 			};
 		});
 	},
-	"_getNextDealer": function() {
+	"_buildPot" function() {
+		var g = games.holdem.game;
+		g._forEachActive(function(p) {
+			g._pot[p._id] = 0;
+		});
 	},
 	"_nextActivePlayerIndex": function(startIndex) {
 		var g = games.holdem.game,
@@ -39,12 +56,17 @@ games.holdem.game =
 				g._nextActivePlayerIndex(startIndex);
 			else return playerIndex;
 	},
-	"_setTableBlinds": function() {
+	"_setNextDealer": function() {
+		var g = games.holdem.game;
+		g._dealer_index = g._nextActivePlayerIndex();
 	},
-
+	"_updateTableBlinds": function() {
+	},
+	"_updateUIs" function() {
+	},
 	"_updatePlayerCard": function(player, card, card_num) {
 		var g = games.holdem.game,
-			playerData = g._data[player.id],
+			playerData = g._data[player._id],
 			cardKey = "card_" + card_num,
 			cardVal = card.val(), update = {};
 		playerData[cardKey] = card;
@@ -56,7 +78,7 @@ games.holdem.game =
 	},
 	"_updateDisplayCard": function(card, card_num) {
 		var g = games.holdem.game,
-			displayData = g._data[g._display.id],
+			displayData = g._data[g._display._id],
 			cardKey = "card_" + card_num,
 			cardVal = card.val(), update = {};
 		displayData[cardKey] = card;
@@ -65,6 +87,94 @@ games.holdem.game =
 		//may want to pass card object to display update
 		//where setCardImage could be attached to the card class instance
 		g._display.update(update);
+	},
+	"_updatePlayersStatus": function() {
+		var g = games.holdem.game, playerData;
+		g._players.forEach(function(p) {
+			playerData = g._display[p._id];
+			if (playerData.stat == "fold")
+				playerData.stat = "active";
+			if (playerData.cash <= 0)
+				playerData.stat = "inactive";
+		});
+	},
+	"_resetRound": function() {
+		var g = games.holdem.game,
+			blindIncs = g._hand_number / g._blind_increase_interval;
+		g._buildPot();
+		g._round_bid = 100 * Math.pow(2, Math.floor(blindIncs));
+	},
+	"_updatePlayerCash": function() {
+		
+	},
+	"_distributePot": function(winners) {
+		var g = games.holdem.game, potTotal = 0, splitTotal,
+			winnerBid, winnerTotals, playerLoss, updateValue;
+		winners.forEach(function(wpid) {
+			winnerTotals[wpid] = 0;
+		});
+		winners.sort(function(id1, id2) {
+			return g._pot[id1] - g._pot[id2];
+		});
+		//assumes winners in increasing order;
+		winners.forEach(function(wpid) {
+			winnerBid = g._pot[wpid];
+			if (winnerBid > 0) {
+				for (var pid in g._pot) {
+					if (winners.indexOf(pid) == -1) {
+						playerLoss = Math.min(g._pot[pid], g._pot[wpid]);
+						potTotal += playerLoss;
+						g._pot[pid] -= playerLoss;
+					}
+				}
+				splitTotal = Math.floor(potTotal/winners.length);
+				winners.forEach(function(wwpid) {
+					if (g._pot[wwpid] > 0) {
+						potTotal -= splitTotal;
+						winnerTotals[wwpid] += splitTotal + g._pot[wwpid];
+						g._pot[wwpid] -= winnerBid;
+					}
+				});
+			}
+		});
+		for (var pid in g._pot) {
+			if (winners.indexOf(pid) != -1)
+				updateValue = winnerTotals[wpid];
+			else if (g._pot[pid] > 0)
+				updateValue = g._pot[pid];
+			else
+				updateValue = 0;
+			g._updatePlayerCash(pid, updateValue);
+		}
+	},
+	"_rankHand": function(player) {
+		var g = games.holdem.game,
+			d = g._data[g._display._id], pid = player._id,
+			p = g._data[pid],
+			playerCards = [
+				p.card_1, p.card_2,
+				d.card_1, d.card_2,
+				d.card_3, d.card_4, d.card_5
+			];
+		g._data[pid].rank = new lib.Rank(playerCards);
+	},
+	"_bestHandRanks": function() {
+		var g = games.holdem.game, highRank,
+			winners = [];
+		g._forEachActive(function(player) {
+			if (winners.length == 0)
+				winners.push(player._id);
+			else if (winners[0].rank._compare(player.rank) < 0)
+				winners = [player._id];
+			else if (winners[0].rank._compare(player.rank) == 0)
+				winners.push(player._id);
+		});
+		return winners;
+	},
+	"_determineWinners": function() {
+		var g = games.holdem.game;
+		g._forEachActive(g._rankHand);
+		g._distributePot(g._bestHandRanks());
 	},
 	
 	"_deal": function() {
@@ -104,37 +214,40 @@ games.holdem.game =
 				g._deck.draw();
 				card = g._deck.draw();
 				g._updateDisplayCard(card, 5);
+			},
+			"show": function() {
+				g._determineWinners();
 			}
 		};
 		d[g._game_stage]();
 	},
 	"_handlePlayerResponse": function(p, resp) {
-		var g = games.holdem.game, cash,
+		var g = games.holdem.game, cash = resp.cash,
 			playerIndex = g._players.indexOf(p),
 			nextPlayerIndex = g._nextActivePlayerIndex(playerIndex);
-		switch (resp.type) {
-			case "raise":
-				cash = resp.raise;
-				g._bid_start_index = playerIndex;
-				g._round_bid = cash;
-				g._data[p._id].cash -= cash;
-				g._round_total += cash;
-				break;
-			case "call":
-				cash = g._round_bid;
-				g._data[p._id].cash -= cash;
-				g._round_total += cash;
-				break;
-			case "fold":
-				g._data[p._id].stat = "fold";
-				break;
-			default:
-				break
-		}
-		if (nextPlayerIndex == g._bid_start_index)
-			g._run();
-		else
+		if (nextPlayerIndex == g._bid_start_index) g._run();
+		else {
+			switch (resp.type) {
+				case "raise":
+					g._bid_start_index = playerIndex;
+					g._round_bid += cash;
+					g._pot[p._id] += g._round_bid;
+					g._data[p._id].cash -= cash;
+					g._round_total += cash;
+					break;
+				case "call":
+					g._pot[p._id] += cash;
+					g._data[p._id].cash -= cash;
+					g._round_total += cash;
+					break;
+				case "fold":
+					g._data[p._id].stat = "fold";
+					break;
+				default:
+					break;
+			}
 			g._bidQuery(g._players[nextPlayerIndex]);
+		}
 	},
 	"_bidQuery": function(p) {
 		var g = games.holdem.game;
@@ -151,13 +264,20 @@ games.holdem.game =
 				g._bidQuery(player);
 			},
 			"flop": function() {
+				g._bidQuery(g._players[g._nextActivePlayerIndex()]);
 			},
 			"turn": function() {
+				g._bidQuery(g._players[g._nextActivePlayerIndex()]);
 			},
 			"river": function() {
+				g._bidQuery(g._players[g._nextActivePlayerIndex()]);
+			},
+			"show": function() {
+				g._end();
 			}
 		};
 		g._round_bid = 0;
+		b[g._game_stage]();
 	},
 	"_run": function() {
 		var g = games.holdem.game;
@@ -166,13 +286,33 @@ games.holdem.game =
 			g._sequence[0];
 		g._deal();
 		g._bid();
+		}
+	},
+	"_clean": function() {
+		var g = games.holdem.game;
+		g._updatePlayersStatus();
+		g._updateUIs();
+	},
+	"_end": funtion() {
+		var g = games.holdem.game;
+		g._clean();
+		g._setNextDealer();
+		g._start();
 	},
 	"_start": function() {
 		var g = games.holdem.game;
-		g._hand_number++;
-		g._setTableBlinds();
-		g._run();
+		g._resetRound();
+		if (g._numActive() == 1)
+			g._forEachActive(function(player) {
+				g._display.setWinner(player);
+			});
+		else {
+			g._hand_number++;
+			g._updateTableBlinds();
+			g._run();
+		}
 	},
+
 	"start": function() {
 		games.holdem.game._start();
 	},
