23f492456787793ec8a46562ef39e3dbdb74e8ae
bubbleboy14 <mario.balibrera@gmail.com>
2016-01-24 19:39:58 -0800

    core.UI constructor adds chat button to view. core.ui: load() builds object of ui instances (instead of discarding old ones); update() routes messages by channel to the appropriate ui (or chat) instance.

--- mobile-controller/js/core/ui.js
+++ mobile-controller/js/core/ui.js
@@ -6,11 +6,13 @@ core.UI = CT.Class({
 		this.view = CT.dom.node("", "div", "fullscreen");
 		this.chat = new core.Chat(name);
 		this.view.appendChild(this.chat.node);
+		this.view.appendChild(this.chat.button);
 	},
 	"update": function() {} // override
 });
 
 core.ui = {
+	"_uis": [],
 	"_ui": null,
 	"view": null,
 	"actor": null,
@@ -23,18 +25,22 @@ core.ui = {
 	},
 	"update": function(d) {
 		if (d.message.action == "chat")
-			core.ui._ui.chat.update(d);
+			core.ui._uis[d.channel].chat.update(d);
 		else
-			core.ui._ui.update(d);
+			core.ui._uis[d.channel].update(d);
 	},
 	"load": function(gamename, obj) {
-		var is_lobby = gamename == "lobby",
-			pf = core.ui.platform,
-			req_base = is_lobby ? "lobby" : ("games." + gamename);
-		core.ui.view.innerHTML = "";
-		CT.require(req_base + ".ui." + pf, true);
-		core.ui._ui = new (is_lobby ? lobby
-			: games[gamename]).ui[pf](obj, gamename);
+		if (!(gamename in core.ui._uis)) {
+			var is_lobby = gamename == "lobby",
+				pf = core.ui.platform,
+				gametype = gamename.split("_")[0],
+				req_base = is_lobby ? "lobby" : ("games." + gametype);
+			core.ui.view.innerHTML = "";
+			CT.require(req_base + ".ui." + pf, true);
+			core.ui._uis[gamename] = new (is_lobby ? lobby
+				: games[gametype]).ui[pf](obj, gamename);
+		}
+		core.ui._ui = core.ui._uis[gamename];
 		core.ui.view.appendChild(core.ui._ui.view);
 	}
 };
