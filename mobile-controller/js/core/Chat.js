a19a507a4b2ef6a40bdf058fdac6c5897a14a7fc
bubbleboy14 <mario.balibrera@gmail.com>
2016-01-24 18:01:07 -0800

    core.Chat: renamed in/out to avoid compilation error

--- mobile-controller/js/core/Chat.js
+++ mobile-controller/js/core/Chat.js
@@ -1,20 +1,20 @@
 core.Chat = CT.Class({
 	"init": function(name) {
 		this.name = name;
-		this.out = CT.dom.node();
-		this.in = CT.dom.smartField(this.say, "fullwidth");
+		this._out = CT.dom.node();
+		this._in = CT.dom.smartField(this.say, "fullwidth");
 		this.node = CT.dom.wrapped([
-			this.out,
-			this.in
+			this._out,
+			this._in
 		], "div", "chat");
 	},
 	"say": function(message) {
 		core.ui.actor.say(this.name, message);
-		this.in.value = "";
+		this._in.value = "";
 	},
 	"update": function(data) {
 		if (data.user == "Concierge") return; // for now ;)
-		this.out.appendChild(CT.dom.node("<b>" + data.user + "</b>: " + data.message.data));
-		this.out.scrollTop = this.out.scrollHeight;
+		this._out.appendChild(CT.dom.node("<b>" + data.user + "</b>: " + data.message.data));
+		this._out.scrollTop = this._out.scrollHeight;
 	}
 });
