e69b5d36d16fc66c7f4170275e1bb1047140e5fe
drmartinlutherxing <drmartinlutherchang@gmail.com>
2016-01-27 19:37:56 -0800

    created _vars object to track game related local variables controlling values on ui; _reponses: object containing functions for respond to channel updates(summary, move, turn, deal); _setCash sets ui._vars.cash; _setCurrentBid sets _vars.current_bid; _setAnte defines cash related variables on _vars; _setBlinds evaluates list containing player ids in order dealer, small_blind, bid_blind. sets btn value based on this if player's account name is in list. updates invested and current bid _vars; changed defined dom _update functions to update based on _vars or _cards values; defines Mobile._update to call node _update functions; modified update to call corresponding _responses function, and call Mobile._update; modified moveCb to return a click callback that modifies local _vars values, and creates the approriate emmision message based on button clicked, and calls Mobile._update; define _acc_name to be core.actor.name; adjusted name of _move to moveCb;

--- mobile-controller/js/games/holdem/ui/Mobile.js
+++ mobile-controller/js/games/holdem/ui/Mobile.js
@@ -4,6 +4,66 @@ CT.require("lib.Deck");
 
 games.holdem.ui.Mobile = CT.Class({
 	consts: games.holdem.constants,
+	_vars: {},
+	_responses: {
+		summary: function(s) {
+			CT.log("games.holdem.ui.Mobile._responses.summary: " + JSON.stringify(s));
+			this._setAnte(s.ante);
+			this._setCash(s.cash[this.pid])
+			this._setBlinds(s.blinds)
+		},
+		move: function(m) {
+			CT.log("games.holdem.ui.Mobile._responses.move: " + JSON.stringify(m));
+		},
+		turn: function(t) {
+			CT.log("games.holdem.ui.Mobile._responses.turn: " + JSON.stringify(t));
+		},
+		deal: function(d) {
+			CT.log("games.holdem.ui.Mobile._responses.deal: " + JSON.stringify(d));
+			var card = new lib.Card(d.suit, d.value);
+			this._cards.push(card);
+			//this["card_" + this._cards.length]._update(card.val());
+		}
+	},
+	_setCash: function(c) {
+		this._vars.cash = c || this._vars.cash;
+		//this.current_money._update(this._cash);
+	},
+	_setCurrentBid: function(c) {
+		this._vars.current_bid = c || 0;
+	},
+	_setAnte: function(a) {
+		this._vars.invested = 0;
+		this._vars.ante = this._vars.round_bid = this._vars.next_bid = a;
+		//this.next_bid._update(this._min_raise);
+	},
+	_setBlinds: function(b) {
+		// assumes b is a list of player ids in order
+		// 0 - dealer, 1 - small, 2 - big
+		var btn, blind = 0;
+		switch (b.indexOf(this.account)) {
+			case 0:
+				btn = "dealer";
+				break;
+			case 1:
+				btn = "small_blind";
+				blind = this.ante / 2;
+				break;
+			case 2:
+				btn = "big_blind";
+				blind = this.ante;
+				break;
+			default:
+				btn = "hidden";
+				break;
+		}
+		this._vars.btn = btn;
+		this._vars.invested += blind;
+		this._setCurrentBid(blind);
+		//this.table_btn._update(btn);
+	},
+	_fold: function() {
+	},
 	_build: function() {
 		this._cards = [];
 		this.view.appendChild(CT.dom.wrapped([
@@ -32,50 +92,57 @@ games.holdem.ui.Mobile = CT.Class({
 	_updates: function() {
 		var that = this, _updates = {
 			"table_btn": function(btn) {
-				that.table_btn.className = "m-holdem_btn " + (btn || "hidden");
+				that.table_btn.className =
+					"m-holdem_btn " + (that._vars.btn || "hidden");
 			},
-			"account_name": function(name) {
-				that.account_name.innerHTML = name;
+			"account_name": function() {
+				that.account_name.innerHTML = that.acc_name;
 			},
-			"current_bid": function(money) {
-				that.current_bid.innerHTML = "$" + money;
+			"current_bid": function() {
+				that.current_bid.innerHTML = "$" + 0;//this._vars.current_bid;
 			},
-			"card_1": function(card) {
-				lib.card.setCardImage(that.card_1, card);
+			"card_1": function() {
+				lib.card.setCardImage(that.card_1, that._cards[0].val());
 			},
-			"card_2": function(card) {
-				lib.card.setCardImage(that.card_2, card);
+			"card_2": function() {
+				lib.card.setCardImage(that.card_2, that._cards[1].val());
 			},
-			"current_money": function(money) {
-				that.current_money.innerHTML = "$" + money;
+			"current_money": function() {
+				that.current_money.innerHTML = "$" + that._vars.cash;
 			},
-			"next_bid": function(money) {
-				that.next_bid.innerHTML = "$" + money;
+			"next_bid": function() {
+				that.next_bid.innerHTML = "$" + that._vars.next_bid;
 			},
-			"raise_button": function(money) {
-				that.raise_button.innerHTML = "$" + money + " RAISE";
+			"raise_button": function() {
+				that.raise_button.innerHTML = "$" + that._vars.next_bid + " RAISE";
 			},
-			"call_button": function(money) {
-				that.call_button.innerHTML = 
-					money ? "$" + money + " CALL" : "CHECK";
+			"call_button": function() {
+				var toCall = that.round_bid - that.invested;
+				if (toCall > 0) {
+					that.call_button.className = "m-holdem_button call";
+					that.call_button.innerHTML = "$" + toCall + " CALL";
+				}else if (toCall == 0) {
+					that.call_button.className = "m-holdem_button check";
+					that.call_button.innerHTML = "CHECK";
+				}
 			},
-			"fold_button": function(fold) {
-				that.fold_button.innerHTML = fold ? "FOLD" : "CHECK / FOLD";
+			"fold_button": function() {
+				var toCall = that._vars.round_bid - that._vars.invested;
+				that.fold_button.innerHTML = toCall > 0 ? "FOLD" : "CHECK / FOLD";
 			}
 		};
-		for (var u in _updates)
-			this[u]._update = _updates[u];
-	},
-	"_deal": function(c) {
-		var card = new lib.Card(c.suit, c.value);
-		this._cards.push(card);
-		this["card_" + this._cards.length]._update(card.val());
+		this._update = function() {
+			//this[u]._update = _updates[u];
+			for (var u in _updates)
+				_updates[u]();
+		};
 	},
 	update: function(u) {
 		CT.log("games.holdem.ui.Mobile.update: " + JSON.stringify(u));
 		var msg = u.message;
-		if (msg.action == "deal")
-			this._deal(msg.data);
+		if (Object.keys(this._responses).indexOf(msg.action) != -1)
+			this._responses[msg.action](msg.data);
+		this._update();
 	},
 	load: function(obj) {
 		CT.log("games.holdem.ui.Mobile.load: " + JSON.stringify(obj));
@@ -86,13 +153,47 @@ games.holdem.ui.Mobile = CT.Class({
 	leave: function(user) {
 		CT.log("games.holdem.ui.Mobile.leave: " + user);
 	},
-	"_move": function(move) {
-		var name = this.name;
-		return function() {
-			core.actor.emit(name, "move", move);
-		};
+	"moveCb": function(m) {
+		//needs checks for whether player has available money
+		var that = this,
+			buttonCb = function() {
+				var move,
+					toCall = that._vars.round_bid - that._vars.invested,
+					toRaise = toCall + that._vars.next_bid,
+					toAll = that._vars.cash - toCall;
+				if (m == "CALL") {
+					if (toCall == 0)
+						move = "CHECK";
+					else if (toCall > 0) {
+						move = "$" + toCall + " " + m;
+						that._vars.round_bid += toCall;
+						that._vars.invested += toCall;
+						that._vars.current_bid += toCall;
+						that._vars.cash -= toCall;
+					}
+				}else if (m == "RAISE") {
+					move = "$" + toRaise + " " + m;
+					that._vars.round_bid += that._vars.next_bid;
+					that._vars.invested += toRaise;
+					that._vars.current_bid += toRaise;
+					that._vars.cash -= toRaise;
+				}else if (m == "ALL-IN") {
+					move = "$" + toAll  + " RAISE";
+					that._vars.round_bid += toAll;
+					that._vars.invested += that._vars.cash;
+					that._vars.current_bid += that._vars.cash;
+					that._setCash(0);
+				}else {
+					move = m;
+					that._fold();
+				}
+				that._update();
+				core.actor.emit(that.name, "move", move);
+			};
+		return buttonCb;
 	},
 	init: function() {
+		this.acc_name = core.actor.name;
 		this.view.classList.add("mobile-background");
 		this.table_btn = CT.dom.node("", "div", "m-holdem_btn dealer");
 		this.account_name = CT.dom.node("", "div", "m-holdem_text account_name");
@@ -104,14 +205,14 @@ games.holdem.ui.Mobile = CT.Class({
 		this.next_bid = CT.dom.node("", "div", "m-holdem_next_bid");
 
 		this.allin_button = CT.dom.button("ALL-IN",
-			this._move("ALL-IN"), "m-holdem_allin_button");
+			this.moveCb("ALL-IN"), "m-holdem_allin_button");
 		//bid_slider: CT.dom.node(),
 		this.raise_button = CT.dom.button("$100 RAISE",
-			this._move("$100 RAISE"), "m-holdem_button raise");
+			this.moveCb("RAISE"), "m-holdem_button raise");
 		this.call_button = CT.dom.button("$100 CALL",
-			this._move("$100 CALL"), "m-holdem_button call");
+			this.moveCb("CALL"), "m-holdem_button call");
 		this.fold_button = CT.dom.button("FOLD",
-			this._move("FOLD"), "m-holdem_button fold");
+			this.moveCb("FOLD"), "m-holdem_button fold");
 
 		this._build();
 //		setTimeout(this.update, 0, games.holdem.initial);
