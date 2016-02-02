2e919ceeed960412316a219c0711697c03a3d09f
drmartinlutherxing <drmartinlutherchang@gmail.com>
2016-02-01 20:17:21 -0800

    wrapped KIND loop syntax; fullHouses typo; added limit on straight check lists to 5; straightener function to be called during while. takes parameter which specifies that the given card is to be treated like a low ace; call straighter again with true if card is ace; sort the Straights based on their first card which should be the largest in the straight, so long as the the ace is not being used as a low ace; fix conditional for 2PAIR; fixed empty list in _filter; set this._available to 0 after sub(); adjust HOUSE _string to account for sorting house cards order; typos availbale to _available; type s1 to s2 ; added more test datas for lower A straights and with from 7 cards, House and 2Kind 2Pair 3Kind, 1KIND;

--- mobile-controller/js/lib/Rank.js
+++ mobile-controller/js/lib/Rank.js
@@ -20,11 +20,12 @@ lib.Rank = CT.Class({
 		"KIND": function(num) {
 			var _ = this._, hasValue, kinds = [],
 				num = num || 1;
-			for (hasValue in _.values)
+			for (hasValue in _.values) {
 				if (num == 1)
 					kinds.push(new lib.Rank([_.values[hasValue][0]]));
 				else if (_.values[hasValue].length == num)
 					kinds.push(new lib.Rank(_.values[hasValue]));
+			}
 
 			kinds.sort(function(a, b) {
 				return b._cards[0].rank - a._cards[0].rank;
@@ -47,7 +48,7 @@ lib.Rank = CT.Class({
 						});
 				});
 			});
-			return fullHouse;
+			return fullHouses;
 		},
 		"FLUSH": function() {
 			var _ = this._, hasSuit, flush, flushs = [];
@@ -83,36 +84,53 @@ lib.Rank = CT.Class({
 								newStraight = s.slice(0, s.length - 1);
 								newStraight.push(currentCard);
 								straights.push(newStraight);
-							}else if (low) {
-								if (lastCard.value == "A")
-									lastRank = -1;
-								if (currentRank == lastRank + 1)
-									s.unshift(currentCard);
-							}else if (currentRank == lastRank - 1)
-								s.push(currentCard);
+							}else if (s.length < 5) {
+								if (low) {
+									if (lastCard.value == "A")
+										lastRank = -1;
+									if (currentRank == lastRank + 1)
+										s.unshift(currentCard);
+								}else if (currentRank == lastRank - 1)
+									s.push(currentCard);
+							}
 						});
 						if (low) --straightIndex;
 						else ++straightIndex;
 					}
+				}, straightener = function(low) {
+					card = rank._cards[index];
+					straights = [[]];
+					straight = straights[0];
+					straight.push(card);
+					findStraights(low);
+					straights.forEach(function(s) {
+						if (s.length == 5) {
+							returnStraights.push(new lib.Rank(s));
+						}
+					});
 				};
-		while (index < 3) {
-			straights = [[]];
-			card = this._cards[index];
-			lowAce = card.value == "A";
-			straight = straights[0];
-			straight.push(card);
-			lastValue = card.value;
-			lastRank = card.rank,
-			findStraights()
-			lowAce && findStraights(lowAce);
-			straights.forEach(function(s) {
-				if (s.length == 5) {
-					returnStraights.push(new lib.Rank(s));
-				}
-			});
-			++index;
-		}
+			while (index < 3) {
+				straightener();
+				card.value == "A" && straightener(true)
+				++index;
+			}
+
 			//sort straights;
+			returnStraights.sort(function(a, b) {
+				var aFirstCard = a._cards[0], aSecondCard = a._cards[1],
+					bFirstCard = b._cards[0], bSecondCard = b._cards[1],
+					aRank = aFirstCard.rank, bRank = bFirstCard.rank;
+					if (aFirstCard.value == "A") {
+						aRank = aSecondCard.rank;
+						a._cards.push(a._cards.shift());
+					}
+					if (bFirstCard.value == "A") {
+						bRank = bSecondCard.rank;
+						b._cards.push(b._cards.shift());
+					}
+				return bRank - aRank;
+			});
+
 			return returnStraights;
 		},
 		"2PAIR": function() {
@@ -120,10 +138,12 @@ lib.Rank = CT.Class({
 				pairs, pairses = [];
 				
 			rank._is("KIND", [2], function(matches) {
-				if (matches.length > 1)
+				if (matches.length > 1) {
 					pairs = matches[0]._cards.concat(matches[1]._cards);
-				pairses.push(new lib.Rank(pairs));
+					pairses.push(new lib.Rank(pairs));
+				}
 			})
+			return pairses;
 		}
 	},
 	"_is": function(t, params, success, failure) {
@@ -137,7 +157,6 @@ lib.Rank = CT.Class({
 	"_fill": function() {
 		var rank = this, index = 0;
 		rank._is("KIND", [1], function(matches) {
-				console.log(matches);
 				rank._string += "1KIND";
 				while (rank._available > 0 && index < matches.length) {
 						rank._available -= 1;
@@ -148,7 +167,8 @@ lib.Rank = CT.Class({
 	},
 	"_filter": function(matched) {
 		var rank = this;
-		return this._cards.filter(function(card) {
+		return !matched ? this._cards :
+			this._cards.filter(function(card) {
 				return matched.indexOf(card) == -1;
 			});
 	},
@@ -156,6 +176,7 @@ lib.Rank = CT.Class({
 		var reducedCards = this._filter(matched),
 			newRank = new lib.Rank(reducedCards, this._available);
 		newRank._fill();
+		this._available = 0;
 		this._string += newRank._string;
 	},
 	"_best": function() {
@@ -170,19 +191,21 @@ lib.Rank = CT.Class({
 						rank._sub(k4[0]._cards);
 					}, function() {
 						rank._is("HOUSE", null, function(h) {
-								rank.available -= 5;
-								rank._string += "HOUSE" + h[0]._cards[0].value
-										+ h[0]._cards[3];
+								var hvals = h[0]._cards[0] == h[0]._cards[2] ?
+									[h[0]._cards[0].value, h[0]._cards[3].value] :
+									[h[0]._cards[2].value, h[0]._cards[0].value];
+								rank._available -= 5;
+								rank._string += "HOUSE" + hvals[0] + hvals[1];
 							}, function() {
 								rank._is("FLUSH", null, function(f) {
-										rank.available -= 5;
+										rank._available -= 5;
 										rank._string += "FLUSH";
 										f[0]._cards.forEach(function(card) {
 											rank._string += card.value;
 										});
 									}, function() {
 										rank._is("STRAIGHT", null, function(strt) {
-												rank.available -= 5;
+												rank._available -= 5;
 												rank._string +=
 													"STRAIGHT" + strt[0]._cards[0].value;
 											}, function() {
@@ -250,7 +273,7 @@ lib.Rank = CT.Class({
 			else if (_r2[i] > _r1[i]) return -1;
 			else if (_r1[i] == _r2[i] && _r1[i] != -1) {
 				s1 = r1.substr(r1.indexOf(order[i]) + order[i].length);
-				s1 = r2.substr(r2.indexOf(order[i]) + order[i].length);
+				s2 = r2.substr(r2.indexOf(order[i]) + order[i].length);
 				for (var j = 0; j < s1.length; ++j) {
 					v1 = this._cards[0]._getRank(s1[j]);
 					v2 = this._cards[0]._getRank(s2[j]);
@@ -284,9 +307,8 @@ lib.Rank._test = function() {
 		"club"
 	], cards = [];
 
-
 	/*
-	// STRAIGHTT FROM
+	// STRAIGHTT FROM 5
 	["6", "9", "8", "7", "T"].forEach(function(v, index) {
 		cards.push(new lib.Card(suits[index % 4], v));
 	});
@@ -294,19 +316,19 @@ lib.Rank._test = function() {
 	console.log(TEST2);
 	TEST2._best();
 	console.log(TEST2);
+	*/
 
+	/*
 	// STRAIGHT_FLUSHA FROM 5
 	cards = [];
 	["T", "J", "Q", "K", "A"].forEach(function(v) {
 		cards.push(new lib.Card("club", v));
 	});
-	TEST = new lib.Rank(cards);
-	console.log(TEST);
+	TEST = new lib.Rank(cards, 5);
 	TEST._best();
 	console.log(TEST);
-	*/
 	
-	// 4KIND FROM 5
+	// 4KIND31KIND2 FROM 5
 	TEST = new lib.Rank([
 			new lib.Card("club", "3"),
 			new lib.Card("diamond", "3"),
@@ -316,18 +338,112 @@ lib.Rank._test = function() {
 		], 5);
 	TEST._best();
 	console.log(TEST);
+*/
 
-	/*
-	// 2 PAIR FROM 5
+	// HOUSE3K FROM 5
 	TEST = new lib.Rank([
+			new lib.Card("club", "3"),
+			new lib.Card("diamond", "3"),
+			new lib.Card("heart", "3"),
+			new lib.Card("spade", "K"),
+			new lib.Card("club", "K")
+		], 5);
+	TEST._best();
+	console.log(TEST);
+
+/*
+	// STRAIGHT7 FROM 7
+	["A", "2","3", "4", "5", "6", "7"].forEach(function(v, index) {
+		cards.push(new lib.Card(suits[index % 4], v));
+	});
+	TEST = new lib.Rank(cards, 5);
+	TEST._best();
+	console.log(TEST);
+	
+
+	// STRAIGHT5 FROM 7
+	["A", "2","3", "3", "4", "4", "5"].forEach(function(v, index) {
+		cards.push(new lib.Card(suits[index % 4], v));
+	});
+	TEST = new lib.Rank(cards, 5);
+	TEST._best();
+	console.log(TEST);
+
+
+	// STRAIGHTT FROM 7
+	cards = [];
+	["4", "5","6", "9", "8", "7", "T"].forEach(function(v, index) {
+		cards.push(new lib.Card(suits[index % 4], v));
+	});
+	TEST2 = new lib.Rank(cards, 5);
+	TEST2._best();
+	console.log(TEST2);
+
+	console.log(TEST.compare(TEST2));
+
+	// 3KIND31KINDQT FROM 5
+	TEST2 = new lib.Rank([
+			new lib.Card("club", "3"),
+			new lib.Card("diamond", "3"),
+			new lib.Card("heart", "3"),
+			new lib.Card("spade", "T"),
+			new lib.Card("club", "Q")
+		], 5);
+	TEST2._best();
+	console.log(TEST2);
+
+	console.log(TEST.compare(TEST2));
+
+	// HOUSE3Q FROM 5
+	TEST2 = new lib.Rank([
+			new lib.Card("club", "3"),
+			new lib.Card("diamond", "3"),
+			new lib.Card("heart", "3"),
+			new lib.Card("spade", "Q"),
+			new lib.Card("club", "Q")
+		], 5);
+	TEST2._best();
+	console.log(TEST2);
+	
+
+	// 2PAIR321KINDK FROM 5
+	TEST2 = new lib.Rank([
 			new lib.Card("club", "K"),
 			new lib.Card("diamond", "3"),
 			new lib.Card("heart", "3"),
 			new lib.Card("heart", "2"),
 			new lib.Card("spade", "2")
 		], 5);
+	TEST2._best();
+	console.log(TEST2);
+
+	// 1KINDAKQ75 FROM 7
+	TEST = new lib.Rank([
+			new lib.Card("heart", "7"),
+			new lib.Card("heart", "5"),
+			new lib.Card("diamond", "Q"),
+			new lib.Card("heart", "K"),
+			new lib.Card("spade", "2"),
+			new lib.Card("heart", "3"),
+			new lib.Card("spade", "A")
+		], 5);
 	TEST._best();
-	console.log(TEST);
+	console.log(TEST._string);
+
 	*/
+	// 2KINDK1KINDAQT FROM 7
+	TEST2 = new lib.Rank([
+			new lib.Card("diamond", "K"),
+			new lib.Card("spade", "T"),
+			new lib.Card("diamond", "Q"),
+			new lib.Card("heart", "K"),
+			new lib.Card("spade", "2"),
+			new lib.Card("heart", "3"),
+			new lib.Card("spade", "A")
+		], 5);
+	TEST2._best();
+	console.log(TEST2._string);
+
+	console.log(TEST.compare(TEST2));
 
 };
