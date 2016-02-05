3719cbeb08f6c3439dffc88ef6fa464cb5e825d0
bubbleboy14 <mario.balibrera@gmail.com>
2016-02-05 11:29:02 -0800

    log config, auto-grep

--- mobile-controller/js/core/config.js
+++ mobile-controller/js/core/config.js
@@ -1,9 +1,15 @@
 core.config = {
 	"botheads": false,
 	"timeout": 30,
+	"log": {
+		"include": [],
+		"exclude": []
+	},
 	"ws": {
 		"host": "localhost",
 		"port": 8888,
 		"reconnect": false
 	}
 };
+
+CT.log.grep(core.config.log.include, core.config.log.exclude);
